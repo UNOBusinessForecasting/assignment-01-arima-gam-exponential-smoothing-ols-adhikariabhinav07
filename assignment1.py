56facc72681ad2a22b4a74859253040bb3851a1b
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-09-12 18:37:15 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,4 @@
+#Import statements
 import pandas as pd
 import numpy as np
 import statsmodels.formula.api as smf
@@ -5,8 +6,8 @@ import statsmodels.tsa.stattools as st
 import plotly.express as px
 
 # Prep the dataset
-data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
+data = pd.read_csv(
+    "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 
-reg = smf.ols("trips ~ Timestamp", data=data)
-reg = reg.fit()
-print(reg.summary())
+model = smf.ols("trips ~ Timestamp", data=data)
+modelfit = reg.fit()
