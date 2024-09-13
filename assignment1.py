c4ebb3d0dd740b4f2a16d8cbec9dc0a7d3e1f0bb
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-09-12 20:40:28 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -8,7 +8,7 @@ import plotly.express as px
 train_data = pd.read_csv(
     "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 eqn = 'trips ~ month+day+hour'
-y, x = pt.dmatrices(eqn, train_data)
+y, x = pd.dmatrices(eqn, train_data)
 model = LinearGAM(s(0) + s(1) + s(2) + s(3) )
 modelFit = model.gridsearch(np.asarray(x), y)
 modelFit.summary()
