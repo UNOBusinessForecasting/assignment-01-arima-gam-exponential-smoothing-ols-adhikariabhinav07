b1d088aaef6af6596c6d0f319c1331bf12d71f33
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-09-12 19:57:10 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -5,9 +5,10 @@ import statsmodels.formula.api as smf
 import statsmodels.tsa.stattools as st
 import plotly.express as px
 
-# Prep the dataset
-data = pd.read_csv(
+train_data = pd.read_csv(
     "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
-
-model = smf.ols("trips ~ Timestamp", data=data)
-modelfit = reg.fit()
+eqn = 'trips ~ month+day+hour'
+y, x = pt.dmatrices(eqn, train_data)
+model = sm.tsa.arima.ARIMA(y, seasonal_order=(1,1,0,24), exog=x)
+reg = model.fit()
+pred = reg.forecast(steps=744, exog=x[-744:]) 
