9fb5a6dc19eb7bc86b71d1c4ce997fee7e756d58
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-09-12 20:43:57 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,11 +4,12 @@ import numpy as np
 import statsmodels.formula.api as smf
 import statsmodels.tsa.stattools as st
 import plotly.express as px
+import patsy as pt
 
 train_data = pd.read_csv(
     "https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 eqn = 'trips ~ month+day+hour'
-y, x = pd.dmatrices(eqn, train_data)
+y, x = pt.dmatrices(eqn, train_data)
 model = LinearGAM(s(0) + s(1) + s(2) + s(3) )
 modelFit = model.gridsearch(np.asarray(x), y)
 modelFit.summary()
