d700617c550461f20470692747544b6e60327408
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-09-18 11:13:36 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,8 +4,8 @@ import numpy as np
 from pygam import LinearGAM, s
 
 train_data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
-eqn = 'trips ~ month+day+hour'
-y, x = pt.dmatrices(eqn, train_data)
+x = train_data[['hour', 'day', 'month']]
+y = train_data['trips']
 model = LinearGAM(s(0) + s(1) + s(2) + s(3) )
 modelFit = model.gridsearch(np.asarray(x), y)
 
