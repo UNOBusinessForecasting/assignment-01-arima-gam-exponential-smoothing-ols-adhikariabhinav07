e0fd12297f8c2c7205e729af3ee812e08fcfdfd9
adhikariabhinav07 <128512549+adhikariabhinav07@users.noreply.github.com>
2024-09-18 11:16:33 -0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -6,7 +6,7 @@ from pygam import LinearGAM, s
 train_data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
 x = train_data[['hour', 'day', 'month']]
 y = train_data['trips']
-model = LinearGAM(s(0) + s(1) + s(2) + s(3) )
+model = LinearGAM(s(0) + s(1) + s(2))
 modelFit = model.gridsearch(np.asarray(x), y)
 
 
